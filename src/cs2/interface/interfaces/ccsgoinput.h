93ec3ecd2371940d78c8a1754b1e195d337d34fc
JacobTheIdiot <105955516+JacobTheIdiot@users.noreply.github.com>
2026-03-07 18:36:37 -0800

    updated ccsgoinput and engine interfaces

--- src/cs2/interface/interfaces/ccsgoinput.h
+++ src/cs2/interface/interfaces/ccsgoinput.h
@@ -8,6 +8,27 @@
 
 #define MULTIPLAYER_BACKUP 150
 
+class CCSInputMessage
+{
+public:
+	std::int32_t m_nFrameTickCount; //0x0000
+	float m_flFrameTickFraction; //0x0004
+	std::int32_t m_nPlayerTickCount; //0x0008
+	float m_flPlayerTickFraction; //0x000C
+	QAngle_t m_angViewAngles; //0x0010
+	Vector_t m_vecShootPosition; //0x001C 
+	std::int32_t m_nTargetIndex; //0x0028
+	Vector_t m_vecTargetHeadPosition; //0x002C What's this???
+	Vector_t m_vecTargetAbsOrigin; //0x0038
+	Vector_t m_vecTargetAngle; //0x0044
+	std::int32_t m_nsvShowHitRegistration; //0x0050
+	std::int32_t m_nEntryIndexMax; //0x0054
+	std::int32_t m_nIndexUnk; //0x0058
+	int idekjustpadding;
+};
+static_assert(sizeof(CCSInputMessage) == 0x60);
+
+
 class CTinyMoveStepData
 {
 public:
@@ -17,6 +38,31 @@ public:
 	bool bPressed; //0x0010
 	MEM_PAD(0x7); //0x0011
 }; //Size: 0x0018
+
+class CSubtickInput {
+public:
+	float m_flWhen;
+	float m_flDelta;
+	uint64_t m_nButton;
+	bool m_bPressed;
+	char pad_0011[3];
+	int m_nMouseInput;
+	Vector_t angles;
+};
+class CButtonData
+{
+public:
+	std::uint64_t flag;
+	char* name;
+	std::byte m_pad0[0x8];
+	float value;
+	std::byte m_pad1[0x4];
+	float when_start;
+	float when_end;
+	std::byte m_pad2[0x4];
+	int command_count;
+	std::byte m_pad3[0x60];
+}; //Size: 0x0048
 
 class CMoveStepButtons
 {
@@ -45,34 +91,53 @@ public:
 class CCSGOInput
 {
 public:
-	MEM_PAD(0x250);
-	CUserCmd arrCommands[MULTIPLAYER_BACKUP];
-	MEM_PAD(0x99)
-	bool bInThirdPerson;
+	char pad_0000[16]; //0x0000
+	CButtonData* buttons_data[63]; //0x0010
+	char pad_0208[72]; //0x0208
+	bool bBlockShot;
+	bool m_bInThirdPerson;
 	MEM_PAD(0x6);
 	QAngle_t angThirdPersonAngles;
-	MEM_PAD(0xE);
-	std::int32_t nSequenceNumber;
-	double dbSomeTimer;
-	CExtendedMoveData currentMoveData;
-	std::int32_t nWeaponSwitchTick;
-	MEM_PAD(0x1C4);
-	CExtendedMoveData* pExtendedMoveData;
-	MEM_PAD(0x48);
+	MEM_PAD(0x14);
+	uint64_t nKeyboardPressed;
+	uint64_t nMouseWheelPressed;
+	uint64_t nUnPressed;
+	uint64_t nKeyboardCopy;
+	float flForwardMove;
+	float flSideMove;
+	float flUpMove;
+	Vector2D_t nMousePos;
+	int32_t SubticksCount;
+	CSubtickInput Subticks[32];
+	Vector_t vecViewAngle;
+	int32_t nTargetHandle;
+	MEM_PAD(0x230);
 	int32_t nAttackStartHistoryIndex1;
 	int32_t nAttackStartHistoryIndex2;
 	int32_t nAttackStartHistoryIndex3;
+	char pad_061C[4]; //0x061C
+	int32_t m_nMessageSize; //0x0620
+	char pad_0624[6]; //0x0624
+	CCSInputMessage* m_pMessage; //0x0630
 
 	CUserCmd* GetUserCmd()
 	{
-		return &arrCommands[nSequenceNumber % MULTIPLAYER_BACKUP];
+		typedef uintptr_t(__fastcall* GetLocalControllerByInternalIdFn)(int);
+		static GetLocalControllerByInternalIdFn GetLocalControllerByInternalId = (GetLocalControllerByInternalIdFn)memory::FindPattern(CLIENT_DLL, "48 83 ec ?? 83 f9 ?? 75 ?? 48 8b 0d ?? ?? ?? ?? 48 8d 54 24 ?? 48 8b 01 ff 90 ?? ?? ?? ?? 8b 08 48 63 c1 48 8d 0d ?? ?? ?? ?? 48 8b 04 c1 48 83 c4 ?? c3 cc cc cc cc cc cc cc cc cc cc cc cc cc 48 83 ec ?? 83 f9");
+		typedef uintptr_t(__fastcall* SetupCmdFn)(uintptr_t);
+		static SetupCmdFn SetupCmd = (SetupCmdFn)memory::FindPattern(CLIENT_DLL, "48 83 EC 28 E8 ?? ?? ?? ?? 8B 80");
+		typedef uintptr_t(__fastcall* GetControllerCmdFn)(uintptr_t, uintptr_t);
+		static GetControllerCmdFn GetControllerCmd = (GetControllerCmdFn)memory::FindPattern(CLIENT_DLL, "40 53 48 83 EC 20 8B DA E8 ?? ?? ?? ?? 4C");
+
+		auto Controller = GetLocalControllerByInternalId(0);
+		return (CUserCmd*)GetControllerCmd(Controller, SetupCmd(Controller));
 	}
 
 	void SetViewAngle(QAngle_t& angView)
 	{
-		// @ida: this got called before GetMatricesForView
+		// xref: str: "bot_takeover" -> the only function in the if statement without a str in the params
 		using fnSetViewAngle = std::int64_t(CS_FASTCALL*)(void*, std::int32_t, QAngle_t&);
-		static auto oSetViewAngle = reinterpret_cast<fnSetViewAngle>(memory::FindPattern(CLIENT_DLL, XOR("85 D2 75 3F 48")));
+		static auto oSetViewAngle = reinterpret_cast<fnSetViewAngle>(memory::FindPattern(CLIENT_DLL, XOR("85 D2 75 ? 48 63 81")));
 
 		#ifdef CS_PARANOID
 		CS_ASSERT(oSetViewAngle != nullptr);
